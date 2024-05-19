20640c47a5357ecc810fdfc3ce234360bfac5033
unknown <you@example.com>
2024-05-19 22:31:12 +0200

    wip

--- main/ethernet_example_main.c
+++ main/ethernet_example_main.c
@@ -20,8 +20,9 @@ static const char *TAG = "eth_example";
 static nvs_handle_t NVS_handle;
 
 typedef struct {
-  int recv_socket;
   void(*dataCallback)(const char*);
+  uint32_t myIpAddr;
+  int port;
 } ethRxLoopArg_t;
 
 // === error manager ===
@@ -60,57 +61,137 @@ int errMan_getError(errMan_t* self, char* dest){
   return retVal;
 }
 
+static void IRAM_ATTR send_to_tcpIp(int send_socket, struct sockaddr* dest_addr, uint8_t* data, int nBytes){
+  int actual_send = sendto(send_socket, data, nBytes, 0, dest_addr, sizeof(struct sockaddr_in));
+  if (actual_send != nBytes) {
+    ESP_LOGI(TAG, "sendto");
+    ESP_ERROR_CHECK(ESP_FAIL);
+  }
+}
+
+
+
 // === ethernet receive loop, shuts down task on disconnect ===
 static IRAM_ATTR void ethRxLoop(void* _arg){
   ethRxLoopArg_t* arg = (ethRxLoopArg_t*)_arg;
-  const uint32_t nBytesMax = 255; // +1 for C string zero termination
-  uint32_t nBytesBuf = 0;
-  uint32_t nParsed = 0;
-  char cmdBuf[256];
-  int state = 0;
-  int overrun = 0;
-  while (1){
-    int blockingRead = !(state & 0x1);
-    uint32_t nBytesNextRead = blockingRead ? 1 : nBytesMax - nBytesBuf;
-    uint32_t flags = blockingRead ? 0 : /*non-blocking*/MSG_DONTWAIT;
 
-    if (nBytesNextRead == 0){
-      if (!overrun){
-	errMan_reportError(&errMan, "max cmd length exceeded");
-	overrun = 1;
-      }
-      nBytesBuf = 0;
-      nBytesNextRead = blockingRead ? 1 : nBytesMax;
-    }
-    
-    int nBytesReceived = recv(arg->recv_socket, cmdBuf, nBytesNextRead, flags);
-    if (nBytesReceived < 0){ // DISCONNECT
-      close(arg->recv_socket);
-      vTaskDelete(NULL);      
-      return;
-    }
-    if (nBytesReceived > nBytesNextRead){
-      ESP_LOGI(TAG, "recvfrom: ?!excess data?!");
+
+  // === socket: create ===
+  int listen_socket = -1;
+  int client_socket = -1;
+  int opt = 1;
+  int err = 0;
+  struct sockaddr_in remote_addr;
+  socklen_t addr_len = sizeof(struct sockaddr);
+  struct sockaddr_storage listen_addr = { 0 };
+  
+  struct sockaddr_in listen_addr4 = { 0 };
+  
+  listen_addr4.sin_family = AF_INET;
+  listen_addr4.sin_port = htons(arg->port);
+  listen_addr4.sin_addr.s_addr = arg->myIpAddr;
+  
+  listen_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+  if (listen_socket < 0){
+    ESP_LOGE(TAG, "Unable to create socket: errno %d", errno);
+    ESP_ERROR_CHECK(ESP_FAIL);
+  }    
+  setsockopt(listen_socket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+  ESP_LOGI(TAG, "Socket created for port %d", arg->port);
+  
+  // === socket: bind ===
+  err = bind(listen_socket, (struct sockaddr *)&listen_addr4, sizeof(listen_addr4));
+  if (err){
+    ESP_LOGE(TAG, "Socket unable to bind: errno %d", errno);
+    ESP_ERROR_CHECK(ESP_FAIL);
+  }
+  
+  // === socket: listen ===
+  err = listen(listen_socket, /*backlog*/5);
+  if (err){
+    ESP_LOGE(TAG, "listen: errno %d", errno);
+    ESP_ERROR_CHECK(ESP_FAIL);
+  }
+  memcpy(&listen_addr, &listen_addr4, sizeof(listen_addr4));
+
+  // === socket accept loop ===
+  while (1){
+    //struct timeval timeout = { 0 };
+    //timeout.tv_sec = IPERF_SOCKET_RX_TIMEOUT;
+    //setsockopt(listen_socket, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout));
+    client_socket = accept(listen_socket, (struct sockaddr *)&remote_addr, &addr_len);
+    if (client_socket < 0){
+      ESP_LOGE(TAG, "accept: errno %d", errno);
       ESP_ERROR_CHECK(ESP_FAIL);
     }
-
-    nBytesBuf += nBytesReceived;
+    ESP_LOGI(TAG, "accept: %s,%d", inet_ntoa(remote_addr.sin_addr), htons(remote_addr.sin_port));
+      
+    //timeout.tv_sec = IPERF_SOCKET_RX_TIMEOUT;
+    //setsockopt(client_socket, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout));
     
-    // === scan for newline \n ===
-    for (int ix = nParsed; ix < nBytesBuf; ++ix){
-      if (cmdBuf[ix] == '\n'){
-	if (overrun){
-	  // error has already been reported but we do salvage data following the newline
-	  overrun = 0;
-	} else {
-	  cmdBuf[ix] = 0; // convert newline char to C end-of-string null
-	  arg->dataCallback(cmdBuf);	  
+    const uint32_t nBytesMax = 255; // +1 for C string zero termination
+    uint32_t nBytesBuf = 0;
+    uint32_t nParsed = 0;
+    char cmdBuf[256];
+    int state = 0;
+    int overrun = 0;
+    while (1){
+      int blockingRead = !(state & 0x1);
+      uint32_t nBytesNextRead = blockingRead ? 1 : nBytesMax - nBytesBuf;
+      uint32_t flags = blockingRead ? 0 : /*non-blocking*/MSG_DONTWAIT;
+
+      if (nBytesNextRead == 0){
+	if (!overrun){
+	  errMan_reportError(&errMan, "max cmd length exceeded");
+	  overrun = 1;
 	}
-	
-	// move data beyond the newline char (pos: +1; length: -1) to the head of buf
-	memcpy(cmdBuf, cmdBuf+ix+1, nBytesBuf-nParsed-1);
-	nParsed -= ix;
+	nBytesBuf = 0;
+	nBytesNextRead = blockingRead ? 1 : nBytesMax;
       }
+
+      ESP_LOGI(TAG, "receiving %d bytes", (int)nBytesNextRead);
+      
+      int nBytesReceived = recv(client_socket, cmdBuf+nBytesBuf, nBytesNextRead, flags);
+      ESP_LOGI(TAG, "got %d bytes", (int)nBytesReceived);
+      if (nBytesReceived < 0){ // DISCONNECT
+	ESP_LOGI(TAG, "disconnect on port %d", arg->port);
+	close(client_socket);
+	vTaskDelete(NULL);      
+	return;
+      }
+      if (nBytesReceived > nBytesNextRead){
+	ESP_LOGI(TAG, "recvfrom: ?!excess data?!");
+	ESP_ERROR_CHECK(ESP_FAIL);
+      }
+
+      nBytesBuf += nBytesReceived;
+      state = (state + 1) & 0x1;
+    
+      // === scan for newline \n ===
+      int ix = nParsed;
+      while (ix < nBytesBuf){
+	if (cmdBuf[ix] != '\n'){
+	  ++nParsed;
+	  ++ix;
+	} else { // newline
+	  if (overrun){
+	    // error has already been reported but we do salvage data following the newline
+	    overrun = 0;
+	  } else {
+	    // echo data (including newline) 
+	    send_to_tcpIp(client_socket, (struct sockaddr *)&remote_addr, /*data*/(uint8_t*)cmdBuf, /*nBytes*/ix);
+	    cmdBuf[ix] = 0; // convert newline char to C end-of-string null
+	    arg->dataCallback(cmdBuf);
+	  }
+	
+	  // move data beyond the newline char (pos: +1; length: -1) to the head of buf
+	  memcpy(cmdBuf, cmdBuf+ix+1, nBytesBuf-nParsed-1);
+	  // after above move, continue scanning from head of buffer
+	  nBytesBuf -= ix;
+	  nParsed -= ix;
+	  ix = 0;
+	} // if newline
+      } // foreach unparsed char
     }
   }
 }
@@ -175,7 +256,7 @@ static void console_task(void *arg){
 
 /** Event handler for Ethernet events */
 static void eth_event_handler(void *arg, esp_event_base_t event_base,
-                              int32_t event_id, void *event_data){
+			      int32_t event_id, void *event_data){
   uint8_t mac_addr[6] = {0};
   /* we can get the ethernet driver handle from event data */
   esp_eth_handle_t eth_handle = *(esp_eth_handle_t *)event_data;
@@ -203,7 +284,7 @@ static void eth_event_handler(void *arg, esp_event_base_t event_base,
 
 /** Event handler for IP_EVENT_ETH_GOT_IP */
 static void got_ip_event_handler(void *arg, esp_event_base_t event_base,
-                                 int32_t event_id, void *event_data){
+				 int32_t event_id, void *event_data){
   ip_event_got_ip_t *event = (ip_event_got_ip_t *) event_data;
   const esp_netif_ip_info_t *ip_info = &event->ip_info;
   
@@ -215,6 +296,7 @@ static void got_ip_event_handler(void *arg, esp_event_base_t event_base,
   ESP_LOGI(TAG, "~~~~~~~~~~~");
 }
 
+#if 0
 static IRAM_ATTR int getc_via_tcpIp(int recv_socket, struct sockaddr* dest_addr, int nBytes, uint8_t* data){
   socklen_t socklen = sizeof(struct sockaddr_in);
   while (1){
@@ -231,205 +313,160 @@ static IRAM_ATTR int getc_via_tcpIp(int recv_socket, struct sockaddr* dest_addr,
     data += nRec;
   }
 }
-
-static void IRAM_ATTR send_to_tcpIp(int send_socket, struct sockaddr* dest_addr, uint8_t* data, int nBytes){
-  int actual_send = sendto(send_socket, data, nBytes, 0, dest_addr, sizeof(struct sockaddr_in));
-  if (actual_send != nBytes) {
-    ESP_LOGI(TAG, "sendto");
-    ESP_ERROR_CHECK(ESP_FAIL);
-  }
-}
-
-void app_main(void){{
-  // === initialize NVS ===
-  ESP_LOGI(TAG, "initializing NVS");
-  esp_err_t err = nvs_flash_init();
-  if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-    // NVS partition was truncated and needs to be erased
-    // Retry nvs_flash_init
-    ESP_ERROR_CHECK(nvs_flash_erase());
-    err = nvs_flash_init();
-  }
-  ESP_ERROR_CHECK( err );
-  
-  // === open NVS ===
-  ESP_LOGI(TAG, "initializing NVS");
-  err = nvs_open("storage", NVS_READWRITE, &NVS_handle);
-  if (err != ESP_OK) {
-    ESP_LOGE(TAG, "nvs_open (%s)", esp_err_to_name(err));
-    ESP_ERROR_CHECK(ESP_FAIL);
-  }
-
-  // == set up default values if not found in NVM ===
-  char* ip= "192.168.178.123";
-  char* gateway = "192.168.178.1";
-  char* netmask = "255.255.255.0";
-  esp_netif_ip_info_t info;
-  memset(&info, 0, sizeof(esp_netif_ip_info_t));
-  info.ip.addr = esp_ip4addr_aton(ip);
-  info.gw.addr = esp_ip4addr_aton(gateway);
-  info.netmask.addr = esp_ip4addr_aton(netmask);
-  
-  err = nvs_get_u32(NVS_handle, "ip", &info.ip.addr);
-  switch (err) {
-  case ESP_OK:
-    ESP_LOGI(TAG, "ip found in nvs");
-    break;
-  case ESP_ERR_NVS_NOT_FOUND:
-    ESP_LOGI(TAG, "no ip in nvs, using default");
-    break;
-  default :
-    ESP_LOGE(TAG, "nvs_get_u32 (%s)", esp_err_to_name(err));
-    ESP_ERROR_CHECK(ESP_FAIL);    
-  }
-
-  err = nvs_get_u32(NVS_handle, "gateway", &info.gw.addr);
-  switch (err) {
-  case ESP_OK:
-    ESP_LOGI(TAG, "gateway found in nvs");
-    break;
-  case ESP_ERR_NVS_NOT_FOUND:
-    ESP_LOGI(TAG, "no gateway in nvs, using default");
-    break;
-  default :
-    ESP_LOGE(TAG, "nvs_get_u32 (%s)", esp_err_to_name(err));
-    ESP_ERROR_CHECK(ESP_FAIL);    
-  }
-
-  err = nvs_get_u32(NVS_handle, "netmask", &info.netmask.addr);
-  switch (err) {
-  case ESP_OK:
-    ESP_LOGI(TAG, "netmask found in nvs");
-    break;
-  case ESP_ERR_NVS_NOT_FOUND:
-    ESP_LOGI(TAG, "no netmask in nvs, using default");
-    break;
-  default :
-    ESP_LOGE(TAG, "nvs_get_u32 (%s)", esp_err_to_name(err));
-    ESP_ERROR_CHECK(ESP_FAIL);    
-  }
-
-  ESP_LOGI(TAG, "ip:" IPSTR, IP2STR(&info.ip));
-  ESP_LOGI(TAG, "netmask:" IPSTR, IP2STR(&info.netmask));
-  ESP_LOGI(TAG, "gateway:" IPSTR, IP2STR(&info.gw));
-}  
-  // Initialize Ethernet driver
-  uint8_t eth_port_cnt = 0;
-  esp_eth_handle_t *eth_handles;
-  ESP_ERROR_CHECK(example_eth_init(&eth_handles, &eth_port_cnt));
-  
-  // Initialize TCP/IP network interface aka the esp-netif (should be called only once in application)
-  ESP_ERROR_CHECK(esp_netif_init());
-  // Create default event loop that running in background
-  ESP_ERROR_CHECK(esp_event_loop_create_default());
-
-  // Create instance(s) of esp-netif for Ethernet(s)
-  if (eth_port_cnt != 1){
-    ESP_LOGI(TAG, "expecting 1 ETH interface, got %d", eth_port_cnt);
-    ESP_ERROR_CHECK(ESP_FAIL);
-  }
-  
-  esp_netif_config_t cfg = ESP_NETIF_DEFAULT_ETH();
-  esp_netif_t *eth_netif = esp_netif_new(&cfg);
-  // Attach Ethernet driver to TCP/IP stack
-  ESP_ERROR_CHECK(esp_netif_attach(eth_netif, esp_eth_new_netif_glue(eth_handles[0])));
-    
-  // Register user defined event handers
-  ESP_ERROR_CHECK(esp_event_handler_register(ETH_EVENT, ESP_EVENT_ANY_ID, &eth_event_handler, NULL));
-  ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_ETH_GOT_IP, &got_ip_event_handler, NULL));
-
-  esp_netif_dhcpc_stop(eth_netif);
-  char* ip= "192.168.178.123";
-  char* gateway = "192.168.178.1";
-  char* netmask = "255.255.255.0";
-  esp_netif_ip_info_t info;
-  memset(&info, 0, sizeof(esp_netif_ip_info_t));
-  info.ip.addr = esp_ip4addr_aton(ip);
-  info.gw.addr = esp_ip4addr_aton(gateway);
-  info.netmask.addr = esp_ip4addr_aton(netmask);
-  esp_netif_set_ip_info(eth_netif, &info);     
-    
-  // Start Ethernet driver state machine
-  ESP_ERROR_CHECK(esp_eth_start(eth_handles[0]));
-  
-  // === socket: create ===
-  int listen_socket = -1;
-  int client_socket = -1;
-  int opt = 1;
-  int err = 0;
-  struct sockaddr_in remote_addr;
-  socklen_t addr_len = sizeof(struct sockaddr);
-  struct sockaddr_storage listen_addr = { 0 };
-  
-  struct sockaddr_in listen_addr4 = { 0 };
-  
-  listen_addr4.sin_family = AF_INET;
-  listen_addr4.sin_port = htons(/*port*/79);
-  listen_addr4.sin_addr.s_addr = info.ip.addr;
-  
-  listen_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-  if (listen_socket < 0){
-    ESP_LOGE(TAG, "Unable to create socket: errno %d", errno);
-    ESP_ERROR_CHECK(ESP_FAIL);
-  }    
-  setsockopt(listen_socket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
-  ESP_LOGI(TAG, "Socket created");
-  
-  // === socket: bind ===
-  err = bind(listen_socket, (struct sockaddr *)&listen_addr4, sizeof(listen_addr4));
-  if (err){
-    ESP_LOGE(TAG, "Socket unable to bind: errno %d", errno);
-    ESP_ERROR_CHECK(ESP_FAIL);
-  }
-  
-  // === socket: listen ===
-  err = listen(listen_socket, /*backlog*/5);
-  if (err){
-    ESP_LOGE(TAG, "listen: errno %d", errno);
-    ESP_ERROR_CHECK(ESP_FAIL);
-  }
-  memcpy(&listen_addr, &listen_addr4, sizeof(listen_addr4));
-  
-  // === start console task ===
-  int ret = xTaskCreatePinnedToCore(console_task, "myConsole", /*stack*/4096, NULL, tskIDLE_PRIORITY, NULL, portNUM_PROCESSORS - 1);
-  if (ret != pdPASS) {
-    ESP_LOGE(TAG, "failed to create console task");
-  }
-#if 0    
-  while (1){
-    char c = fgetc(stdin);
-    if (c != 0xFF){
-      fprintf(stdout, "%02x\r\n", (int)c);
-      break;
-    }
-  }
-  fprintf(stdout, "thank you\r\n");
 #endif
-  // === socket accept loop ===
-  while (1){
-    //struct timeval timeout = { 0 };
-    //timeout.tv_sec = IPERF_SOCKET_RX_TIMEOUT;
-    //setsockopt(listen_socket, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout));
-    client_socket = accept(listen_socket, (struct sockaddr *)&remote_addr, &addr_len);
-    if (client_socket < 0){
-      ESP_LOGE(TAG, "accept: errno %d", errno);
+
+void myCallback(const char* string){
+}
+ 
+void app_main(void){{
+    // === initialize NVS ===
+    ESP_LOGI(TAG, "initializing NVS");
+    esp_err_t err = nvs_flash_init();
+    if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      // NVS partition was truncated and needs to be erased
+      // Retry nvs_flash_init
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      err = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK( err );
+
+    {
+      // === open NVS ===
+      ESP_LOGI(TAG, "initializing NVS");
+      err = nvs_open("storage", NVS_READWRITE, &NVS_handle);
+      if (err != ESP_OK) {
+	ESP_LOGE(TAG, "nvs_open (%s)", esp_err_to_name(err));
+	ESP_ERROR_CHECK(ESP_FAIL);
+      }
+
+      // == set up default values if not found in NVM ===
+      char* ip= "192.168.178.123";
+      char* gateway = "192.168.178.1";
+      char* netmask = "255.255.255.0";
+      esp_netif_ip_info_t info;
+      memset(&info, 0, sizeof(esp_netif_ip_info_t));
+      info.ip.addr = esp_ip4addr_aton(ip);
+      info.gw.addr = esp_ip4addr_aton(gateway);
+      info.netmask.addr = esp_ip4addr_aton(netmask);
+  
+      err = nvs_get_u32(NVS_handle, "ip", &info.ip.addr);
+      switch (err) {
+      case ESP_OK:
+	ESP_LOGI(TAG, "ip found in nvs");
+	break;
+      case ESP_ERR_NVS_NOT_FOUND:
+	ESP_LOGI(TAG, "no ip in nvs, using default");
+	break;
+      default :
+	ESP_LOGE(TAG, "nvs_get_u32 (%s)", esp_err_to_name(err));
+	ESP_ERROR_CHECK(ESP_FAIL);    
+      }
+
+      err = nvs_get_u32(NVS_handle, "gateway", &info.gw.addr);
+      switch (err) {
+      case ESP_OK:
+	ESP_LOGI(TAG, "gateway found in nvs");
+	break;
+      case ESP_ERR_NVS_NOT_FOUND:
+	ESP_LOGI(TAG, "no gateway in nvs, using default");
+	break;
+      default :
+	ESP_LOGE(TAG, "nvs_get_u32 (%s)", esp_err_to_name(err));
+	ESP_ERROR_CHECK(ESP_FAIL);    
+      }
+
+      err = nvs_get_u32(NVS_handle, "netmask", &info.netmask.addr);
+      switch (err) {
+      case ESP_OK:
+	ESP_LOGI(TAG, "netmask found in nvs");
+	break;
+      case ESP_ERR_NVS_NOT_FOUND:
+	ESP_LOGI(TAG, "no netmask in nvs, using default");
+	break;
+      default :
+	ESP_LOGE(TAG, "nvs_get_u32 (%s)", esp_err_to_name(err));
+	ESP_ERROR_CHECK(ESP_FAIL);    
+      }
+
+      ESP_LOGI(TAG, "ip:" IPSTR, IP2STR(&info.ip));
+      ESP_LOGI(TAG, "netmask:" IPSTR, IP2STR(&info.netmask));
+      ESP_LOGI(TAG, "gateway:" IPSTR, IP2STR(&info.gw));
+    }
+    // Initialize Ethernet driver
+    uint8_t eth_port_cnt = 0;
+    esp_eth_handle_t *eth_handles;
+    ESP_ERROR_CHECK(example_eth_init(&eth_handles, &eth_port_cnt));
+  
+    // Initialize TCP/IP network interface aka the esp-netif (should be called only once in application)
+    ESP_ERROR_CHECK(esp_netif_init());
+    // Create default event loop that running in background
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    // Create instance(s) of esp-netif for Ethernet(s)
+    if (eth_port_cnt != 1){
+      ESP_LOGI(TAG, "expecting 1 ETH interface, got %d", eth_port_cnt);
       ESP_ERROR_CHECK(ESP_FAIL);
     }
-    ESP_LOGI(TAG, "accept: %s,%d", inet_ntoa(remote_addr.sin_addr), htons(remote_addr.sin_port));
-      
-    //timeout.tv_sec = IPERF_SOCKET_RX_TIMEOUT;
-    //setsockopt(client_socket, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout));
-      
-    // === echo loop ===
+  
+    esp_netif_config_t cfg = ESP_NETIF_DEFAULT_ETH();
+    esp_netif_t *eth_netif = esp_netif_new(&cfg);
+    // Attach Ethernet driver to TCP/IP stack
+    ESP_ERROR_CHECK(esp_netif_attach(eth_netif, esp_eth_new_netif_glue(eth_handles[0])));
+    
+    // Register user defined event handers
+    ESP_ERROR_CHECK(esp_event_handler_register(ETH_EVENT, ESP_EVENT_ANY_ID, &eth_event_handler, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_ETH_GOT_IP, &got_ip_event_handler, NULL));
+
+    esp_netif_dhcpc_stop(eth_netif);
+    char* ip= "192.168.178.123";
+    char* gateway = "192.168.178.1";
+    char* netmask = "255.255.255.0";
+    esp_netif_ip_info_t info;
+    memset(&info, 0, sizeof(esp_netif_ip_info_t));
+    info.ip.addr = esp_ip4addr_aton(ip);
+    info.gw.addr = esp_ip4addr_aton(gateway);
+    info.netmask.addr = esp_ip4addr_aton(netmask);
+    esp_netif_set_ip_info(eth_netif, &info);     
+    
+    // Start Ethernet driver state machine
+    ESP_ERROR_CHECK(esp_eth_start(eth_handles[0]));
+
+
+    ethRxLoopArg_t ethLoopArg;
+    ethLoopArg.dataCallback = myCallback;
+    ethLoopArg.port = 79;
+    ethLoopArg.myIpAddr = info.ip.addr;
+    int ret = xTaskCreatePinnedToCore(ethRxLoop, "eth", /*stack*/4096, (void*)&ethLoopArg, tskIDLE_PRIORITY, NULL, portNUM_PROCESSORS - 1);
+    if (ret != pdPASS) {
+      ESP_LOGE(TAG, "failed to create eth task");
+    }
+
+
+
+
+
+  
+    // === start console task ===
+     ret = xTaskCreatePinnedToCore(console_task, "myConsole", /*stack*/4096, NULL, tskIDLE_PRIORITY, NULL, portNUM_PROCESSORS - 1);
+    if (ret != pdPASS) {
+      ESP_LOGE(TAG, "failed to create console task");
+    }
+
     while (1){
-      uint8_t buf[1];
-      int state = getc_via_tcpIp(client_socket, (struct sockaddr *)&remote_addr, /*nBytes*/1, buf);
-      if (!state){
-	ESP_LOGI(TAG, "disconnect");
-	break; // disconnect
+      printf("zzz\n");    
+      vTaskDelay(1000/portTICK_PERIOD_MS);
+    }
+    
+#if 0    
+    while (1){
+      char c = fgetc(stdin);
+      if (c != 0xFF){
+	fprintf(stdout, "%02x\r\n", (int)c);
+	break;
       }
-      send_to_tcpIp(client_socket, (struct sockaddr *)&remote_addr, /*data*/buf, /*nBytes*/1);
-    }    
+    }
+    fprintf(stdout, "thank you\r\n");
+#endif
 #if 0
   exit:
     if (client_socket != -1) {
